730a1977302c3d5435c76362595502471c9cc810
Tokubara <1270802817@qq.com>
2021-05-01 11:25:20 +0800

    增大了buffer的大小

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <assert.h>
+#define BUFFER_SIZE 32
 
-int buffer;
+int buffer[BUFFER_SIZE];
+int c_pos=0, p_pos=0;
 int counter = 0; // initially, empty
 
 pthread_cond_t cond_c = PTHREAD_COND_INITIALIZER;
@@ -11,17 +13,18 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void put(int value)
 {
-    assert(counter == 0);
-    counter = 1;
-    buffer = value; // 这两句得用锁保护起来, 确保捆绑执行
+    assert(counter<BUFFER_SIZE);
+    counter++;
+    p_pos=(p_pos+1)%BUFFER_SIZE;
+    buffer[p_pos] = value; // 这两句得用锁保护起来, 确保捆绑执行
 }
 
 int get()
 {
-    assert(counter == 1);
-    int ret_buffer = buffer;
-    counter = 0;
-    return ret_buffer;
+    assert(counter > 0);
+    counter--;
+    c_pos=(c_pos+1)%BUFFER_SIZE;
+    return buffer[c_pos];
 }
 
 void *producer(void *arg)
@@ -29,7 +32,7 @@ void *producer(void *arg)
     int *ntasks = (int *)arg;
     for ( int i = 0; i < *ntasks; ++i ) {
       pthread_mutex_lock(&mutex);
-      while(counter==1) {
+      while(counter==BUFFER_SIZE) {
         pthread_cond_wait(&cond_p, &mutex);
       }
       put(i);
